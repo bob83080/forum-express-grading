2ff1d4310967ad00f86b1b0e03f17d35d4c0793f
Bob Huo <bob83080@gmail.com>
2021-04-02 20:01:34 +0800

    Initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
 
@@ -8,6 +9,7 @@ app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 
 app.listen(port, () => {
+  db.sequelize.sync()
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
