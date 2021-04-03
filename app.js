8239d12999d0956f4b0d2e8150cd508af5325ea2
Bob Huo <bob83080@gmail.com>
2021-04-04 00:43:06 +0800

    Add feature: signInPage, signIn and logout

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./models') // 引入資料庫
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const app = express()
 const port = 3000
 
@@ -13,6 +14,10 @@ app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引�
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 
 app.use((req, res, next) => {
@@ -26,6 +31,6 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
