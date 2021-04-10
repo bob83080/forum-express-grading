4cf803b5b4bf7115e467c8cb4711e8cf8915e5fd
Bob Huo <bob83080@gmail.com>
2021-04-11 02:21:19 +0800

    Add delete comment feature for admin

--- routes/index.js
+++ routes/index.js
@@ -59,7 +59,7 @@ module.exports = (app, passport) => {
 
 
   app.post('/comments', authenticated, commentController.postComment)
-
+  app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 
 
 
