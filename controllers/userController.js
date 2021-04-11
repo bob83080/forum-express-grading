90778e93624572bff3afd13a2b61c3367fd49bea
Bob Huo <bob83080@gmail.com>
2021-04-11 16:43:19 +0800

    Add comment restaurant in profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
 const Restaurant = db.Restaurant
+const Comment = db.Comment
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const defaultProfilePic = 'https://i.stack.imgur.com/34AD2.jpg'
@@ -57,30 +58,6 @@ const userController = {
       })
     }
   },
-  // signUp: (req, res) => {
-  //   // confirm password
-  //   if (req.body.passwordCheck !== req.body.password) {
-  //     req.flash('error_messages', '兩次密碼輸入不同！')
-  //     return res.redirect('/signup')
-  //   } else {
-  //     // confirm unique user
-  //     User.findOne({ where: { email: req.body.email } }).then(user => {
-  //       if (user) {
-  //         req.flash('error_messages', '信箱重複！')
-  //         return res.redirect('/signup')
-  //       } else {
-  //         User.create({
-  //           name: req.body.name,
-  //           email: req.body.email,
-  //           password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-  //         }).then(user => {
-  //           req.flash('success_messages', '成功註冊帳號！')
-  //           return res.redirect('/signin')
-  //         })
-  //       }
-  //     })
-  //   }
-  // },
 
   signInPage: (req, res) => {
     return res.render('signin')
@@ -96,13 +73,29 @@ const userController = {
     req.logout()
     res.redirect('/signin')
   },
-  getUser: async (req, res) => {
-    return User.findByPk(req.params.id).then(user => {
 
-      return res.render('userProfile', {
-        userNow: user.toJSON()
-      })
+  getUser: async (req, res) => {
+
+    const result = await Comment.findAndCountAll({
+      raw: true,
+      nest: true,
+      where: {
+        userId: req.params.id
+      },
+      include: Restaurant,
+      distinct: true,
     })
+    const count = result.count
+    const comments = result.rows
+    console.log('result', result)
+    console.log('result', result)
+    return User.findByPk(req.params.id)
+      .then(user => {
+        res.render('userProfile', {
+          userNow: user.toJSON(), count, comments
+        })
+      })
+
   },
 
   editUser: async (req, res) => {
